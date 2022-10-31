4a8155ba4d8b21b51646206732f73ad733b0c551
VishnuReddy0007 <vishnu@flexydial.in>
2022-10-31 10:08:55 +0530

    updating ami key

--- Firsttfscript.tf
+++ Firsttfscript.tf
@@ -1,11 +1,11 @@
 provider "aws"{
     region = "ap-south-1"
-  //  access_key = "AKIA74"
-  //  secret_key = "xYKbAaRGsIPcdQDwPlmde"
+  //  access_key = "*******"
+  //  secret_key = "******"
 }
 
 resource "aws_instance"   "AWSEC2Instance"{
-    ami = "ami-05c8ca4485f8b138a"
+    ami = "***********"
     instance_type = "t2.micro"
     security_groups = ["launch-wizard-"5] ///pree defined sequrity group name had to mention
     key_name = "vishnureddy"
